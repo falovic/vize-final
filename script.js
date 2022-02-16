065b9f5d5d2a8ea97584b733a44350eb0392b0b5
falovic <mail@falovic.com>
2022-02-17 01:26:43 +0300

    update

--- script.js
+++ script.js
@@ -2,45 +2,55 @@
 var vize = 50;
 var final = 80;
 var ortalama = (vize * 0.3) + (final * 0.7);
+var harf;
+var durum;
 
-if (ortalama >= 0 && ortalama < 35) {
-    var harf = "FF";
-    var durum = `Kaldınız`;
-}
+if (ortalama >= 0 && ortalama <= 100) {
 
-else if (ortalama >= 35 && ortalama < 45) {
-    var harf = "DD";
-    var durum = `Koşullu geçtiniz`;
-}
+    if (ortalama >= 80) {
+        harf = "AA";
+        durum = `Geçtiniz`;
+    }
 
-else if (ortalama >= 45 && ortalama < 50) {
-    var harf = "DC";
-    var durum = `Geçtiniz`;
-}
+    else if (ortalama >= 71) {
+        harf = "BA";
+        durum = `Geçtiniz`;
+    }
 
-else if (ortalama >= 50 && ortalama < 55) {
-    var harf = "CC";
-    var durum = `Geçtiniz`;
-}
+    else if (ortalama >= 63) {
+        harf = "BB";
+        durum = `Geçtiniz`;
+    }
 
-else if (ortalama >= 55 && ortalama < 63) {
-    var harf = "CB";
-    var durum = `Geçtiniz`;
-}
+    else if (ortalama >= 55) {
+        harf = "CB";
+        durum = `Geçtiniz`;
+    }
 
-else if (ortalama >= 63 && ortalama < 71) {
-    var harf = "BB";
-    var durum = `Geçtiniz`;
-}
+    else if (ortalama >= 50) {
+        harf = "CC";
+        durum = `Geçtiniz`;
+    }
 
-else if (ortalama >= 71 && ortalama < 80) {
-    var harf = "BA";
-    var durum = `Geçtiniz`;
-}
+    else if (ortalama >= 45) {
+        harf = "DC";
+        durum = `Geçtiniz`;
+    }
 
-else if (ortalama >= 80 && ortalama <= 100) {
-    var harf = "AA";
-    var durum = `Geçtiniz`;
+    else if (ortalama >= 35) {
+        harf = "DD";
+        durum = `Koşullu geçtiniz`;
+    }
+
+    else if (ortalama >= 0) {
+        harf = "FF";
+        durum = `Kaldınız`;
+    }
+
+} else {
+    ortalama = "-";
+    harf = "-";
+    durum = "-";
 }
 
 function push() {
